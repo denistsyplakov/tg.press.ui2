5c8b2b72fea2025e9024a7214b9c4052f3b5d7cb
Denis Tsyplakov <denis.tsyplakov@dataart.com>
2023-08-04 16:40:11 +0400

    layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AboutComponent} from "./cmp/about/about.component";
+import {ChatListComponent} from "./cmp/chat-list/chat-list.component";
+import {Page404Component} from "./cmp/page404/page404.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ChatListComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'chat-list', component: ChatListComponent },
+  { path: '**', component: Page404Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {ClarityModule} from "@clr/angular";
+import { AboutComponent } from './cmp/about/about.component';
+import { ChatListComponent } from './cmp/chat-list/chat-list.component';
+import { Page404Component } from './cmp/page404/page404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ChatListComponent,
+    Page404Component
   ],
   imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ClarityModule,
     BrowserModule,
     AppRoutingModule
   ],
